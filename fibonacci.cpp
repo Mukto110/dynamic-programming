6e7d311c5e53fa5a3d4cc2ce05988d59be1e1296
mukto110 <merajhossainmukto@gmail.com>
2025-03-03 15:04:21 +0600

    practicing dynamic programming

---
+++ fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n)
+{
+    if (n < 2)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    cout << fibonacci(6) << endl;
+    return 0;
+}
